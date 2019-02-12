f1208ddefcdd1b30941d135565efc5633c7e5213
Maia Coghlan <maiaccog@gmail.com>
2019-02-12 12:29:32 +0800

    add remember me function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    p_s = params[:session]
+    user = User.find_by(email: p_s[:email].downcase)
+    if user && user.authenticate(p_s[:password])
       log_in user
+      p_s[:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,8 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user_id)
-    @current_user = nil
+    log_out if logged_in?
     redirect_to root_path
   end
 end
